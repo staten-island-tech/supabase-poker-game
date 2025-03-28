bb0fb35fd978fd698dc6ea0b48f5460622fa6c91
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2025-03-28 12:20:04 -0400

    a

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    
+    <NavBar/>
   </div>
 </template>
 
 <script setup>
-
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import NavBar from './components/NavBar.vue';
 </script>
 
 <style scoped>
