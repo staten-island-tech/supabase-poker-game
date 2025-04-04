636ba3ce0f1ca664e8bdfcfedac5a143e3113706
taskdire <136046239+taskdire@users.noreply.github.com>
2025-04-04 12:33:48 -0400

    adw
    awd

--- app/src/components/NavBar.vue
+++ app/src/components/NavBar.vue
@@ -8,7 +8,7 @@
         <div class="flex space-x-4">
           <SignUp />
           <RouterLink to="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</RouterLink>
-          <RouterLink to="/about" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">About
+          <RouterLink to="/about" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Play Game
           </RouterLink>
         </div>
       </div>
